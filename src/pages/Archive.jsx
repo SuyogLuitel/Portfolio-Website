3a6f888b5b730cad3a6f0ee43bf49b13c3f5dced
SuyogLuitel <suyogluitel235@gmail.com>
2024-10-30 21:57:42 +0545

    archive page fixes

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -6,7 +6,7 @@ const Archive = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-slate-900 w-full h-screen p-32">
+    <div className="bg-slate-900 w-full h-screen px-28 py-20">
       <div className="flex flex-col gap-1">
         <p
           onClick={() => navigate("/")}
@@ -20,22 +20,22 @@ const Archive = () => {
         </h1>
       </div>
 
-      <div className="mt-20">
-        <div className="flex justify-between items-center text-slate-200 leading-normal text-lg font-medium border-b border-slate-800 pb-2">
+      <div className="mt-16">
+        <div className="flex justify-between items-center text-slate-200 leading-normal text-base font-medium border-b border-slate-800 pb-2">
           <div className="flex-1 text-center">Year</div>
           <div className="flex-1 text-center">Project</div>
           <div className="flex-1 text-center">Made at</div>
-          <div className="flex-[2] text-center">Built with</div>{" "}
+          <div className="flex-[3] text-center">Built with</div>{" "}
           <div className="flex-1 text-center">Link</div>
         </div>
 
-        <div className="flex justify-between items-center text-slate-200 leading-normal text-lg font-medium my-5 border-b border-slate-800 pb-2">
+        <div className="flex justify-between items-center text-slate-200 leading-normal text-base font-medium my-5 border-b border-slate-800 pb-2">
           <div className="flex-1 text-center text-sm text-[#94A3B8]">2023</div>
           <div className="flex-1 text-center">Emerson Collective</div>
           <div className="flex-1 text-center text-sm text-[#94A3B8]">
             Upstatement
           </div>
-          <div className="flex-[2] flex items-center gap-3 justify-center flex-wrap">
+          <div className="flex-[3] flex items-center gap-3 justify-center flex-wrap">
             <div className="bg-teal-400/10 text-sm font-medium leading-5 rounded-full px-4 py-2 text-teal-300">
               JavaScript
             </div>
